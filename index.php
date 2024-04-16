2fe426477f9ed0ec623974e32d87882116f1c407
Giammarco Talò <giammarcotalo@gmail.com~>
2024-04-16 21:35:04 +0200

    prima traccia svolta

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//* TRACCIA 1
+
+$integer = 30;
+$float = 12.50;
+$string = 'ciao';
+$boolean = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($boolean);
+
+const INTEGER = 30;
+const FLOAT = 12.50;
+const STRING = 'ciao';
+const BOOLEAN = true;
+
+            
+            
+            ?>
